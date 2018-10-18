73251e8d142cc43b7530d4ad01826ed1290eada7
Gabriele Guerrisi <gglab.center@gmail.com>
2018-10-18 10:59:03 +0200

    Update dataset_building.py

--- dataset_building.py
+++ dataset_building.py
@@ -5,7 +5,7 @@ import numpy as np
 input_dotbrackets = []
 input_sequences = []
 input_ids = []
-len_limit = 700 # 2000 è l'ideale
+len_limit = 700
 
 input_list = os.listdir('data')
 shuffle(input_list)
@@ -33,11 +33,9 @@ for filename in input_list:
   d = ''.join(dotbracket)
   
   if(len(s)<len_limit):
-    input_sequences.append(s)
-    input_dotbrackets.append(d)
+    input_sequences.append(s+'\n')
+    input_dotbrackets.append(d+'\n')
     input_ids.append(filename)
-    
-print("number of raw sequences in input: {}".format(len(input_sequences))) # total: 3554 sequences
 
 token_index_seq = {}
 token_index_db = {}
@@ -51,21 +49,13 @@ for db_sequence in input_dotbrackets:
   for base in db_sequence:
     if base not in token_index_db:
       token_index_db[base] = len(token_index_db)+1
-
+      
 print("characters in the sequences dictionary (full): {}".format(len(token_index_seq)))
 print("characters in the dotbrackets dictionary (full): {}".format(len(token_index_db)))
-print()
-sorted_by_value = sorted(token_index_seq.items(), key=lambda kv: kv[1])
-print("dictionary for sequences")
-print(sorted_by_value)
-print()
-sorted_by_value = sorted(token_index_db.items(), key=lambda kv: kv[1])
-print("dictionary for dotbrackets")
-print(sorted_by_value)
 
 max_length = len(max(input_sequences, key=len))
-print('Filtering now the dictionary in order to remove the seqs with the chars not allowed; allowed values are:')
-allowed_dictionary_seq = ['A', 'C', 'G', 'U', 'M', 'N', 'R', 'W', 'S', 'Y', 'K', 'V', 'H', 'D', 'B']
+allowed_dictionary_seq = ['\n', 'A', 'C', 'G', 'U', 'M', 'N', 'R', 'W', 'S', 'Y', 'K', 'V', 'H', 'D', 'B']
+
 print(allowed_dictionary_seq)
 print()
 input_sequences_temp = []
@@ -101,25 +91,59 @@ input_sequences = input_sequences_temp
 input_dotbrackets = input_dotbrackets_temp
 input_ids = input_ids_temp
 
-input_x = np.zeros((len(input_sequences), max_length, max(token_index_seq_final.values())+1))
+
+base_to_idx = set()
+db_to_idx = set()
+for sequence in input_sequences:
+  for base in sequence:
+    if (base not in base_to_idx):
+      base_to_idx.add(base)
+for sequence in input_dotbrackets:
+  for dbr in sequence:
+    if (dbr not in db_to_idx):
+      db_to_idx.add(dbr)
+base_to_idx = sorted(list(base_to_idx))
+db_to_idx = sorted(list(db_to_idx))
+base_to_idx_dict = {}
+idx_to_base_dict = {}
+for k, v in enumerate(base_to_idx):
+    idx_to_base_dict[k] = v
+    base_to_idx_dict[v] = k
+db_to_idx_dict = {}
+idx_to_db_dict = {}
+for k, v in enumerate(db_to_idx):
+    idx_to_db_dict[k] = v
+    db_to_idx_dict[v] = k  
+max_len_sequence = max([len(line) for line in input_sequences])
+max_len_db = max([len(line) for line in input_dotbrackets])
+base_to_idx_dict
+print()
+print()
+idx_to_db_dict
+print()
+max_len_sequence
+print()
+max_len_db
+
+input_x = np.zeros((len(input_sequences), max_len_sequence, len(base_to_idx)))
 
 for i, sequence in enumerate(input_sequences):
   for j, base in enumerate(sequence):
-    index = token_index_seq_final.get(base)
+    index = base_to_idx_dict.get(base)
     input_x[i, j, index] = 1
     if j == len(sequence)-1:
-      if j != max_length-1:
-        input_x[i, j+1:max_length, 0] = 1
+      if j != max_len_sequence-1:
+        input_x[i, j+1:max_len_sequence, 0] = 1
         
-input_y = np.zeros((len(input_dotbrackets), max_length, max(token_index_db.values()) + 1))
+input_y = np.zeros((len(input_dotbrackets), max_len_db, len(db_to_idx)))
 
 for i, sequence in enumerate(input_dotbrackets):
   for j, base in enumerate(sequence):
-    index = token_index_db.get(base)
+    index = db_to_idx_dict.get(base)
     input_y[i, j, index] = 1
     if j == len(sequence)-1:
-      if j != max_length-1:
-        input_y[i, j+1:max_length, 0] = 1
+      if j != max_len_db-1:
+        input_y[i, j+1:max_len_db, 0] = 1
         
 input_shape = input_x[0].shape
 output_shape = input_y[0,0,:].shape[0]
